1598167bde5a234978f9606511149e5d2a5bc085
neotaso <neotaso.0609@gmail.com>
2021-08-31 02:59:57 +0900

    using https

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var run = function(){
-    fetch("http://character-sheets.appspot.com/satasupe/display?ajax=1&key=ahVzfmNoYXJhY3Rlci1zaGVldHMtbXByFwsSDUNoYXJhY3RlckRhdGEYzfuK3AMM&callback=myFunction").then(
+    fetch("https://character-sheets.appspot.com/satasupe/display?ajax=1&key=ahVzfmNoYXJhY3Rlci1zaGVldHMtbXByFwsSDUNoYXJhY3RlckRhdGEYzfuK3AMM&callback=myFunction").then(
         res =>{        
         console.log(res);
         document.getElementById("tArea").value = res;
