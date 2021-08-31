8c26046e21a54b582a0faedffd360244a6c7054f
neotaso <neotaso.0609@gmail.com>
2021-08-31 20:51:05 +0900

    ver 0.1

--- js/main.js
+++ js/main.js
@@ -1,12 +1,282 @@
-var run = function(){
-    var sc = document.createElement("script");
-    sc.src = 
-    "https://character-sheets.appspot.com/satasupe/display?ajax=1&key=ahVzfmNoYXJhY3Rlci1zaGVldHMtbXByFwsSDUNoYXJhY3RlckRhdGEYzfuK3AMM&callback=myFunc";
-    var parent = document.getElementsByTagName("script")[0];
-    parent.parentNode.insertBefore(sc,parent);
-}
+// 趣味リスト
+const favorites = {
+  "hobby.1_2": "アブノーマル",
+  "hobby.1_3": "カワイイ",
+  "hobby.1_4": "トンデモ",
+  "hobby.1_5": "マニア",
+  "hobby.1_6": "ヲタク",
+  "hobby.2_1": "音楽",
+  "hobby.2_3": "トレンド",
+  "hobby.2_4": "読書",
+  "hobby.2_5": "パフォーマンス",
+  "hobby.2_6": "美術",
+  "hobby.3_1": "アラサガシ",
+  "hobby.3_2": "おせっかい",
+  "hobby.3_4": "家事",
+  "hobby.3_5": "ガリ勉",
+  "hobby.3_6": "健康",
+  "hobby.4_1": "アウトドア",
+  "hobby.4_2": "工作",
+  "hobby.4_3": "スポーツ",
+  "hobby.4_5": "ハイソ",
+  "hobby.4_6": "旅行",
+  "hobby.5_1": "育成",
+  "hobby.5_2": "サビシガリヤ",
+  "hobby.5_3": "ヒマツブシ",
+  "hobby.5_4": "宗教",
+  "hobby.5_6": "ワビサビ",
+  "hobby.6_1": "アダルト",
+  "hobby.6_2": "飲食",
+  "hobby.6_3": "ギャンブル",
+  "hobby.6_4": "ゴシップ",
+  "hobby.6_5": "ファッション",
+};
 
-var myFunc = function(jsonData){
-    console.log(jsonData);
-    document.getElementsById("tArea").value = jsonData;
-}
+const charaCommands = `-汎用-
+SR{性業値} 【性業値判定】
+
+{犯罪}R>=[,,]
+{生活}R>=[,,]
+{恋愛}R>=[,,]
+{教養}R>=[,,]
+{戦闘}R>=[,,]
+{肉体}R>=[,,]
+{精神}R>=[,,]
+
+{肉体}R>=X[,1,13]【セーブ判定】
+{肉体}R>=X[1,2,13] 【セーブ判定(跳ぶ)】
+{肉体}R>=7[1,1,13]  【バッドトリップ判定(酒)】
+{教養}R>=X[4,1,13] 【リンク判定(教養)】
+CultureIET 【教養イベント表】
+CultureIHT 【教養ハプニング表】
+
+-アイテム-
+{攻撃力}R>=X[,1,13] 【武器(ダ+)】
+
+-異能-
+
+-代償-
+`;
+
+var run = function () {
+  var target = document.getElementById("targetUrl").value;
+  // target URL format: https://character-sheets.appspot.com/satasupe/edit.html?key=XXXXXX
+  // "key" regular expression: \w{64}
+
+  // TODO: URL Error Check
+
+  var params = new URL(target).searchParams;
+  var key = params.get("key");
+
+  var sc = document.createElement("script");
+  sc.type = "text/javascript";
+  sc.src = `https://character-sheets.appspot.com/satasupe/display?ajax=1&key=${key}&callback=myFunc`;
+  var parent = document.getElementsByTagName("script")[0];
+  parent.parentNode.insertBefore(sc, parent);
+};
+
+var charaData = function (jsonData) {
+  let character = {
+    kind: "character",
+    data: {
+      name: jsonData.base.name,
+      memo: `【PL名】${jsonData.base.player}
+【PC名】${jsonData.base.name}
+【犯罪】${jsonData.base.abl.crime.value}\
+【生活】${jsonData.base.abl.life.value}\
+【恋愛】${jsonData.base.abl.love.value}\
+【教養】${jsonData.base.abl.culture.value}\
+【戦闘】${jsonData.base.abl.combat.value}
+【肉体】${jsonData.base.gift.body.value}\
+【精神】${jsonData.base.gift.mind.value}
+【性業値】${jsonData.base.emotion}
+【反応力】${jsonData.base.power.initiative}\
+【攻撃力】${jsonData.base.power.attack}\
+【破壊力】${jsonData.base.power.destroy}`,
+      initiative: parseInt(jsonData.base.power.initiative),
+      externalUrl: "",
+      status: [
+        {
+          label: "肉体点",
+          value: 10 - parseInt(jsonData.cond.body.value),
+          max: 10,
+        },
+        {
+          label: "精神点",
+          value: 10 - parseInt(jsonData.cond.mental.value),
+          max: 10,
+        },
+        {
+          label: "サイフ",
+          value:
+            parseInt(jsonData.base.abl.life.value) -
+            parseInt(jsonData.cond.wallet.value),
+          max: parseInt(jsonData.base.abl.life.value),
+        },
+      ],
+      params: [
+        {
+          label: "犯罪",
+          value: jsonData.base.abl.crime.value,
+        },
+        {
+          label: "生活",
+          value: jsonData.base.abl.life.value,
+        },
+        {
+          label: "恋愛",
+          value: jsonData.base.abl.love.value,
+        },
+        {
+          label: "教養",
+          value: jsonData.base.abl.culture.value,
+        },
+        {
+          label: "戦闘",
+          value: jsonData.base.abl.combat.value,
+        },
+        {
+          label: "肉体",
+          value: jsonData.base.gift.body.value,
+        },
+        {
+          label: "精神",
+          value: jsonData.base.gift.mind.value,
+        },
+        {
+          label: "攻撃力",
+          value: jsonData.base.power.attack,
+        },
+        {
+          label: "破壊力",
+          value: jsonData.base.power.destroy,
+        },
+        {
+          label: "性業値",
+          value: jsonData.base.emotion,
+        },
+      ],
+      width: 12,
+      height: 12,
+      commands: charaCommands,
+    },
+  };
+  document.getElementById("chara").value = JSON.stringify(character);
+
+  // 趣味
+  let fav = jsonData.learned
+    .filter((v) => v.id != null)
+    .reduce((p, v) => p + favorites[v.id] + "\n", "");
+
+  // 異能
+  let talent = jsonData.karma
+    .filter((v) => v.talent.name != null)
+    .reduce((p, v) => p + v.talent.name + "\n", "");
+  // 代償
+  let price = jsonData.karma
+    .filter((v) => v.price.name != null)
+    .reduce((p, v) => p + v.price.name + "\n", "");
+
+  let data = `\
+PC名 ${jsonData.base.name}
+年齢 ${jsonData.base.age} 性別 ${jsonData.base.sex}
+好み ${jsonData.base.favorites}
+
+-趣味-
+${fav}
+-異能-
+${talent}
+-代償-
+${price}
+-その他-  
+`;
+  document.getElementById("data").value = data;
+};
+
+var equipment = function (jsonData) {
+  // 一般装備
+  let limit = jsonData.outfitstotal.limit;
+  let weapons = jsonData.weapons
+    .filter((v) => v.name != null && v.place == null)
+    .map((v) => v.name);
+  let outfits = jsonData.outfits
+    .filter((v) => v.name != null && v.place == null)
+    .map((v) => v.name);
+  let items = weapons.concat(outfits);
+  let vehicle = jsonData.vehicles[0];
+  if (items.length < limit) {
+    let oldlen = items.length;
+    items.length = limit;
+    items.fill("", oldlen);
+    if (vehicle.name != null) {
+      items[items.length - 1] = vehicle.name;
+    } else if(vehicle.name != null){
+        items.push(vehicle.name);
+    }
+  }
+
+  let item =
+    `装備(上限${limit})\n` +
+    items.reduce((p, c, i) => p + `${i + 1}. ${c}\n`, "");
+
+  document.getElementById("item").value = item;
+};
+
+var vehicleEquipment = function (jsonData) {
+  // 乗物/乗物装備
+  let vehicle = jsonData.vehicles[0];
+  let burdenWeapons = jsonData.weapons
+    .filter((v) => v.name != null && v.place == "乗物")
+    .map((v) => v.name);
+  let burdenOutfits = jsonData.outfits
+    .filter((v) => v.name != null && v.place == "乗物")
+    .map((v) => v.name);
+  let burdenItems = burdenWeapons.concat(burdenOutfits);
+  if (burdenItems.length < vehicle.burden) {
+    let oldlen = burdenItems.length;
+    burdenItems.length = vehicle.burden;
+    burdenItems.fill("", oldlen);
+  }
+
+  let vehicles = `乗物\n(ス${vehicle.speed ?? "X"}, 車${
+    vehicle.frame ?? "X"
+  }, 荷${vehicle.burden ?? "X"})\n`;
+  if (vehicle.burden != null) {
+    vehicles =
+      vehicles + burdenItems.reduce((p, c, i) => p + `${i + 1}. ${c}\n`, "");
+  } else {
+    vehicles = vehicles += "1. \n2. \n3. \n4. \n5. \n6. ";
+  }
+
+  document.getElementById("vehicle").value = vehicles;
+};
+
+var hideoutEquipment = function (jsonData) {
+  let hideoutWeapons = jsonData.weapons
+    .filter((v) => v.name != null && v.place == "アジト")
+    .map((v) => v.name);
+  let hideoutOutfits = jsonData.outfits
+    .filter((v) => v.name != null && v.place == "アジト")
+    .map((v) => v.name);
+  let hideoutItems = hideoutWeapons.concat(hideoutOutfits);
+  if (hideoutItems.length < 10) {
+    let oldlen = hideoutItems.length;
+    hideoutItems.length = 10;
+    hideoutItems.fill("", oldlen);
+  }
+
+  let area = jsonData.home.place;
+  let hideout = `アジト(${area})\n`;
+  hideout =
+    hideout + hideoutItems.reduce((p, c, i) => p + `${i + 1}. ${c}\n`, "");
+
+  document.getElementById("hideout").value = hideout;
+};
+
+var myFunc = function (jsonData) {
+  console.log(jsonData);
+  charaData(jsonData);
+  equipment(jsonData);
+  vehicleEquipment(jsonData);
+  hideoutEquipment(jsonData);
+};
