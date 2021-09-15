b6402f84d07de83706910b494cf90ed50df2de74
neotaso <neotaso.0609@gmail.com>
2021-09-16 04:12:49 +0900

    fix: method name and logic

--- js/bookmarklet-chrome.js
+++ js/bookmarklet-chrome.js
@@ -2,9 +2,12 @@ navigator.clipboard.readText()
     .then((a) => {
         i = document.getElementById('outfits').tBodies[0].children.length;
         a.split(/\r\n|\n/).forEach(e => {
-            addRow('outfits');
-            document.getElementById(`outfits.${('000' + i).slice(-3)}.name`).value = e.replace(/^(\d.)+\s*/, "");
-            i++;
+            let name = e.replace(/^(\d.)+\s*/, "");
+            if (name != "") {
+                addRow('outfits');
+                document.getElementById(`outfits.${('000' + i).slice(-3)}.name`).value = name;
+                i++;
+            }
         });
     }
     )
--- js/paste-equipment.js
+++ js/paste-equipment.js
@@ -1,26 +1,29 @@
-var pasteEquipment = function () {
-    let paste = document.getElementById('pasteEquipment');
+var pasteEquipmentsFunc = function () {
+    paste = document.getElementById('pasteEquipments');
     i = document.getElementById('outfits').tBodies[0].children.length;
     paste.value.split(/\r\n|\n/).forEach(e => {
-        addRow('outfits');
-        document.getElementById(`outfits.${('000' + i).slice(-3)}.name`).value = e.replace(/^(\d.)+\s*/, "");
-        i++;
+        let name = e.replace(/^(\d.)+\s*/, "");
+        if (name != "") {
+            addRow('outfits');
+            document.getElementById(`outfits.${('000' + i).slice(-3)}.name`).value = name;
+            i++;
+        }
     });
     paste.value = "";
 };
 
 if (location.hostname == "character-sheets.appspot.com") {
     let pasteEquipDiv = document.createElement('div');
-    pasteEquipDiv.setAttribute('id', "div:pasteEquipment");
+    pasteEquipDiv.setAttribute('id', "div:pasteEquipments");
 
     let textarea = document.createElement('textarea');
-    textarea.setAttribute('id', "pasteEquipment");
+    textarea.setAttribute('id', "pasteEquipments");
     textarea.setAttribute('style', "width:200px; height:120px")
     pasteEquipDiv.appendChild(textarea);
 
     let button = document.createElement('input');
     button.setAttribute('type', "button");
-    button.setAttribute('onclick', "pasteEquipment();");
+    button.setAttribute('onclick', "pasteEquipmentsFunc();");
     button.setAttribute('value', "一般装備に追加");
     pasteEquipDiv.appendChild(button);
 
